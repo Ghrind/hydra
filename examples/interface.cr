5ed24c1efeb1ad652ea4c3248ad7fdbbbe76617e
Benoît Dinocourt <ghrind@gmail.com>
2017-10-19 13:30:50 +0200

    Add a way to stop an application

--- examples/interface.cr
+++ examples/interface.cr
@@ -11,13 +11,15 @@ eh.bind("*", "logger", "debug")
 v = Hydra::View.new(x: 50, y: 100)
 eh.register("view", v.event_interface)
 
-a = Hydra::Application.new(v, eh)
+a = Hydra::Application.build(v, eh)
+eh.register("application", a.event_interface)
+
 prompt_1 = Hydra::Prompt.build("prompt-1")
 prompt_1.hide
 eh.register("prompt-1", prompt_1.event_interface)
 v.add_element(prompt_1)
 eh.bind("keypress.c", "prompt-1", "show")
-eh.bind("prompt-1.submit", "application", "exit")
+eh.bind("prompt-1.submit", "application", "stop")
 eh.bind("keypress.enter", "prompt-1", "submit")
 
 a.start
--- src/hydra/application.cr
+++ src/hydra/application.cr
@@ -1,12 +1,38 @@
+require "./event_interface"
 module Hydra
+  class ApplicationEventInterface < EventInterface
+    def initialize(target : Application)
+      @target = target
+    end
+    def trigger(behavior : String, payload : Hash(Symbol, String)) : Array(String)
+      if behavior == "stop"
+        @target.stop
+        Array(String).new
+      else
+        super
+      end
+    end
+  end
   class Application
+    property :event_interface
+    @event_interface : ApplicationEventInterface
+
+    # Workaround for the inability to use self in an initializer
+    # https://github.com/crystal-lang/crystal/issues/4436
+    def self.build(view : Hydra::View, event_hub : Hydra::EventHub)
+      instance = new(view, event_hub)
+      instance.event_interface = ApplicationEventInterface.new(instance)
+      instance
+    end
     def initialize(view : Hydra::View, event_hub : Hydra::EventHub)
       @view = view
+      @event_interface = uninitialized ApplicationEventInterface
       @event_hub = event_hub
     end
     def start
       @view.render
-      loop do
+      @running = true
+      while @running
         char = @view.getch
         sleep 0.01
         next unless char >= 0
@@ -14,6 +40,10 @@ module Hydra
         @view.render
       end
       Crt.done
+    end
+
+    def stop
+      @running = false
     end
   end
 end
